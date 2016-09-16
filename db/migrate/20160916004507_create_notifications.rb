066e8740f0ef4e32de35e6262ea2382133218da9
uu <giladbarkan@live.com>
2016-09-15 21:07:20 -0400

    added model and migration for notification

---
+++ db/migrate/20160916004507_create_notifications.rb
@@ -0,0 +1,8 @@
+class CreateNotifications < ActiveRecord::Migration[5.0]
+  def change
+    create_table :notifications do |t|
+      t.text :content
+      t.belongs_to :user, foreign_key: true
+    end
+  end
+end
